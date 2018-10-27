6a7e8ff2b742f049104bf984167cd188f79c4c93
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-10-27 18:05:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,10 +4,13 @@ module FormsLab
   class App < Sinatra::Base
     
     get '/' do
-      erb :root
+      erb :root 
+    end
     
     get '/new' do
       erb :new
     end
+    
+    
   end
 end
